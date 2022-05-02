c233fb0b6629d0aa2cb70d4a8e9867b0856d01b3
brachintosh <bedendo.br@gmail.com>
2022-05-02 10:20:50 -0300

    pop-up-alter-at-landing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+// import checkMark from './assets/logo-check.png'
+import Swal from 'sweetalert2'
 
 function App() {
+  //  Pop up after loading the page:
+  Swal.fire({
+    title: 'Wellcome!',
+    text: 'Do you want to enter?',
+    icon: 'success',
+    confirmButtonText: 'Check it!'
+  })
+
   return (
     <div className="App">
       <div>
         Hello react!
       </div>
+
+
     </div>
   );
 }
